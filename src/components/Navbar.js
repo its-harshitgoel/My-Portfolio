ce8a25a11c72b16dcb613d42afc4f88632ef5d42
its-harshitgoel <its.harshitgoel@gmail.com>
2024-05-29 13:21:19 +0530

    navbar and typewriter

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+  const location = useLocation();
+
+  return (
+    <nav className="navbar">
+      <a className="hs">
+      
+        HARSHIT GOEL {"</>"}
+      </a>
+      <div>
+        <ul id="navbar">
+          <li>
+            <Link className={location.pathname === '/' ? 'active' : ''} to="/">Home</Link>
+          </li>
+          <li>
+            <Link className={location.pathname === '/about' ? 'active' : ''} to="/about">About</Link>
+          </li>
+          <li>
+            <Link className={location.pathname === '/projects' ? 'active' : ''} to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link className={location.pathname === '/skills' ? 'active' : ''} to="/skills">Skills</Link>
+          </li>
+          <li>
+            <Link className={location.pathname === '/contact' ? 'active' : ''} to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/ParticleBackground.js
@@ -0,0 +1,105 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticleBackground = () => {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+        background: {
+          color: {
+            value: "#000000",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 100,
+            enable: true,
+            opacity: 0.5,
+            width: 0.6,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+                value_area: 2000,
+            },
+            value: 150,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default ParticleBackground;
